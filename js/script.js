c5a355ba8a09ec12f24abb6ad8a61125f740e6eb
Lightning Farron <kevinnarain94@gmail.com>
2020-07-03 17:18:25 +0200

    Quotes object and console.log success

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+console.log('test');
+
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
@@ -10,7 +12,16 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  { quote: `"Words aren't the only way to tell someone how you feel."`, source: `- Tifa Lockhart` },
+  { quote: `"So what if it seems hopeless? If it were me. I still wouldn't give up. Embrace your dreams"`, source: `- Zack Fair` },
+  { quote: `"I have a dream."`, source: `- Martin Luther King Jr.` },
+  { quote: `"Life is not a problem to be solved, but a reality to be experienced."`, source: `- Soren Kierkegaard` },
+  { quote: `"It's not a question of can or can't. There are some things in life you just do."`, source: `- Lightning Farron` }
+];
 
+// test to see if I can print the quotes variable.
+console.log(quotes);
 
 
 /***
